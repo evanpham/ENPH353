baf6856a0e3be9dd7091e84230898601ec0a942e
Evan Pham <fizzer@enph353.JoDo>
2019-09-24 08:19:32 -0700

    getImage node can now display raw camera feed from gazebo

--- src/enph353_ros_lab/nodes/getImage.py
+++ src/enph353_ros_lab/nodes/getImage.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Image
+import cv2
+import numpy as np
 
 
 def callback(data):
     rospy.loginfo("  height: %s  width: %s", data.height, data.width)
-    print(data.data)
+    image = np.fromstring(data.data, dtype='uint8').reshape((data.height, data.width, 3))
+    print(image)
+    cv2.imshow("frame", image)
+    cv2.waitKey(25)
 
 
 def listener():
     rospy.init_node('vidFeed', anonymous=True)
 
-    rospy.Subscriber('/bot/camera1/image_raw', Image, callback)
+    sub = rospy.Subscriber('/bot/camera1/image_raw', Image, callback)
 
     rospy.spin()
 
