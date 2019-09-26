8ec0359223e01b663c62c83c6cf7b5572e2b8350
Evan Pham <pham@enph353.JoDo>
2019-09-25 18:08:01 -0700

    Broke up some functions for cleanliness

--- src/enph353_ros_lab/nodes/getImage.py
+++ src/enph353_ros_lab/nodes/getImage.py
@@ -6,6 +6,29 @@ import cv2
 import numpy as np
 
 
+def getCenter(image):
+	# Crop image
+	crop_img = image[500:1000, 0:1000]
+	# Grayscale
+	gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+	# Gaussian blur
+	blur = cv2.GaussianBlur(gray, (5, 5), 0)
+	# Colour thresh
+	ret, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV)
+
+	# Find center
+	M = cv2.moments(thresh)
+	try:
+		cX = int(M["m10"]/M["m00"])
+		cY = int(M["m01"]/M["m00"])
+	except:
+		print("ahhhhhhhh")
+		cX = 100000
+		cY = 0
+
+	return (cX, cY)
+
+
 def callback(data):
 	# Get image dimensions
 	h = data.height
@@ -17,31 +40,13 @@ def callback(data):
 	# Find image matrix
 	img = np.fromstring(data.data, dtype='uint8').reshape((h, w, 3))
 
-	# Crop image
-	crop_img = img[500:1000, 0:1000]
-	# grayscale
-	gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-	# Gaussian blur
-	blur = cv2.GaussianBlur(gray, (5, 5), 0)
-	# colour thresh
-	ret, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV)
-	# find contours
-	# contours,hierarchy= cv2.findContours(thresh.copy(), 1, \
-	# CV_RETR_EXTERNAL)
-	# edges = cv2.Canny(thresh, 60,255)
-	M = cv2.moments(thresh)
-	try:
-		cX = int(M["m10"]/M["m00"])
-		cY = int(M["m01"]/M["m00"])
-	except:
-		print("ahhhhhhhh")
-		cX = 100000
-		cY = 0
-	cv2.circle(img, (cX, cY+250), 10, (255, 255, 0), -1)
+	center = getCenter(img)
+
+	cv2.circle(img, (center[0], center[1]+250), 10, (255, 255, 0), -1)
 	# Display the resulting frame
 	cv2.imshow('img', img)
 	cv2.waitKey(25)
-	talker(cX)
+	talker(center[0], w)
 
 
 def listener():
@@ -53,7 +58,7 @@ def listener():
 	rospy.spin()
 
 
-def talker(x):
+def talker(x, width):
 
 	kp = -.003
 
@@ -66,7 +71,7 @@ def talker(x):
 		vel_msg.linear.z = 0
 		vel_msg.angular.x = 0
 		vel_msg.angular.y = 0
-		vel_msg.angular.z = (x-500)*kp
+		vel_msg.angular.z = (x-width/2)*kp
 		rate.sleep()
 		pub.publish(vel_msg)
 
