Added PID node for automatic pid adjustment, changed the window frame for the cropped image to take a more centered frame to make the center of mass calculation more correct, added off the line global variable so that it can correct itself if it falls off

src/enph353_ros_lab/nodes/getImage.py
@@ -2,15 +2,18 @@
 import rospy
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
 import cv2
 import numpy as np
 
-global xLast = 0
+
 
 
 def getCenter(image, width, height):
+	global xLast
+	global size
 	# Crop image
-	crop_img = image[height-100:height, 0:width]
+	crop_img = image[height-550:height-250, 0:width]
 	# Grayscale
 	gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
 	# Gaussian blur
@@ -18,19 +21,38 @@ def getCenter(image, width, height):
 	# Colour thresh
 	ret, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV)
 
+	#cv2.imshow('crop img', crop_img)
+	#cv2.imshow('blur img', blur)
+
+
 	# Find center
 	M = cv2.moments(thresh)
-	cX, cY = -1, -1
+	cX, cY = 0, 0
 	try:
 		cX = int(M["m10"]/M["m00"])
 		cY = int(M["m01"]/M["m00"])
 	except:
 		print("ahhhhhhhh")
+		if xLast > 400:
+			side = 1
+			cX = 800
+		else:
+			side = -1
+			cX = 0 
+		
+		
+
+	xLast = cX
+	state(cX)
 
 	return (cX, cY)
 
 
 def callback(data):
+	global xLast
+
+	setpoint()
+
 	# Get image dimensions
 	h = data.height
 	w = data.width
@@ -42,10 +64,10 @@ def callback(data):
 	img = np.fromstring(data.data, dtype='uint8').reshape((h, w, 3))
 
 	center = getCenter(img, w, h)
-	cv2.circle(img, (xLast, center[1]+250), 10, (255, 255, 0), -1)
+	cv2.circle(img, (xLast, 450), 10, (255, 255, 0), -1)
 
 	# Display the resulting frame
-	cv2.imshow('img', img)
+	#cv2.imshow('img', img)
 	cv2.waitKey(25)
 
 	if (center[0] == -1):
@@ -64,25 +86,47 @@ def listener():
 	rospy.Subscriber('/rrbot/camera1/image_raw', Image, callback)
 	rospy.spin()
 
+def setpoint():
+	#This bit of code continuosly sends our desired state to be 
+	#middle of the screen
+	setpoint_pub = rospy.Publisher('/setpoint', Float64, queue_size=10)
+	setpoint = Float64()
+	setpoint.data = 400.0
+	setpoint_pub.publish(setpoint)
+
+def state(x):
+	#This code will send the current state to pid
+	state_pub = rospy.Publisher('/state', Float64, queue_size=10)
+	state = Float64()
+	state.data = x
+	state_pub.publish(state)
+
+def controlEffort(val):
+	global effort
+	effort = val.data
+	print(effort)
 
 def talker(x, width):
-
-	kp = 0.005
-	kd = 0.002
-	derivative = xLast - x
+	global xLast
+	global effort
+	
+	rospy.Subscriber('control_effort', Float64, controlEffort)
 
 	pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 	rate = rospy.Rate(10)  # 10hz
 	if not rospy.is_shutdown():
 		vel_msg = Twist()
-		vel_msg.linear.x = .20
+		vel_msg.linear.x = 0.05
 		vel_msg.linear.y = 0
 		vel_msg.linear.z = 0
 		vel_msg.angular.x = 0
 		vel_msg.angular.y = 0
-		vel_msg.angular.z += (width/2-x)*kp + derivative*kd
+		vel_msg.angular.z = 0.01*effort
 		rate.sleep()
 		pub.publish(vel_msg)
 
 if __name__ == '__main__':
+	xLast = 0
+	effort = 0
+	side = 1
 	listener()