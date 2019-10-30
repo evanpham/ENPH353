55225e7412b38820ee94c3a522a81cd9bd9207e7
Evan Pham <pham@enph353.JoDo>
2019-10-30 14:34:01 -0700

    Added competition world

---
+++ build/2019F_competition_students/adeept_awr/adeept_awr_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pham/enph353_ws/src/2019F_competition_students/adeept_awr/adeept_awr_description
+# Build directory: /home/pham/enph353_ws/build/2019F_competition_students/adeept_awr/adeept_awr_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/pham/enph353_ws/src
+# Build directory: /home/pham/enph353_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("2019F_competition_students/adeept_awr/adeept_awr")
+subdirs("2019F_competition_students/adeept_awr/adeept_awr_description")
+subdirs("2019F_competition_students/enph353/enph353")
+subdirs("2019F_competition_students/enph353/enph353_npcs")
+subdirs("2019F_competition_students/enph353/enph353_utils")
+subdirs("2019F_competition_students/adeept_awr_ros_driver")
+subdirs("2019F_competition_students/adeept_awr/adeept_awr_gazebo")
+subdirs("2019F_competition_students/enph353/enph353_gazebo")
