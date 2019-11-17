fe44171c17ba83c60951398c5972cbb894f351d4
Evan Pham <pham@enph353.JoDo>
2019-11-17 12:33:52 -0800

    doesnt kill pedestrains (often)

--- src/2019F_competition_students/enph353/enph353_gazebo/nodes/line_follower.py
+++ src/2019F_competition_students/enph353/enph353_gazebo/nodes/line_follower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 # from std_msgs.msg import Float64
@@ -8,30 +9,49 @@ import time
 import sys
 import cv2
 import numpy as np
+from scipy import stats
 
 
 class LineFollower:
 
     def __init__(self):
+        self.team = "123"
+        self.password = '456'
         self.cam_path = '/R1/pi_camera/image_raw'
         self.vel_pub = rospy.Publisher('/R1/skid_vel', Twist, queue_size=1)
+        self.plate_pub = rospy.Publisher('/license_plate', String, queue_size=1)
+        self.register()
         self.initial_move()
         self.listener = rospy.Subscriber(self.cam_path, Image, self.callback)
         self.data = None
+        self.lastCross = time.time()
         self.slice_num = 30
         self.frame = Image()
         self.bridge = CvBridge()
 
         time.sleep(5)
 
+    def register(self):
+        registration = self.team + ',' + self.password + ',0,AB12'
+        print(registration)
+        self.plate_pub.publish(registration)
+
     def initial_move(self):
         print("first moves")
-        self.move("L")
+        # self.move("LL")
+        # time.sleep(.5)
+        # self.move("F")
+        # time.sleep(.25)
+        # self.move("LL")
+        # time.sleep(1)
+        # print("moved")
+        # self.stop()
+        self.move("LL")
         time.sleep(.5)
         self.move("F")
-        time.sleep(.25)
-        self.move("L")
-        time.sleep(1)
+        time.sleep(.5)
+        self.move("LL")
+        time.sleep(2)
         print("moved")
         self.stop()
 
@@ -43,12 +63,17 @@ class LineFollower:
             vel_cmd.linear.z = 0
             vel_cmd.angular.z = 0.0
         elif action == "L":  # LEFT
-            vel_cmd.linear.x = 0.0
+            vel_cmd.linear.x = 0.2
             vel_cmd.angular.z = 0.5
         elif action == "R":  # RIGHT
+            vel_cmd.linear.x = 0.2
+            vel_cmd.angular.z = -0.5
+        elif action == "LL":  # LEFT
+            vel_cmd.linear.x = 0.0
+            vel_cmd.angular.z = 0.5
+        elif action == "RR":  # RIGHT
             vel_cmd.linear.x = 0.0
             vel_cmd.angular.z = -0.5
-        print(vel_cmd)
         self.vel_pub.publish(vel_cmd)
 
     def stop(self):
@@ -88,16 +113,49 @@ class LineFollower:
                 state_num = i
 
         cv2.circle(self.frame, (w*state_num/self.slice_num, h-10), 10, (0, 255, 0), -1)
-        print(state_num)
-        # cv2.imshow("frame", bw)
-        # cv2.waitKey(25)
-        self.follow(state_num)
+        # if (self.atCrosswalk(self.frame) and time.time()-self.lastCross > 5):
+        #     self.stop()
+        #     self.dontKillThePedestrian(gray)
+        # else:
+        #     self.follow(state_num)
+        # cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        cv2.imshow("frame", self.frame)
+        cv2.waitKey(25)
+        # self.follow(state_num)
+
+    def dontKillThePedestrian(self, image):
+        bw = cv2.threshold(image, 220, 255, cv2.THRESH_BINARY)[1]
+        corner = bw[300:400, 750:850]
+        # cornerG = image[300:400, 750:850]
+        # cv2.imshow("dontkill", corner)
+        # cv2.imshow("dontkill2", cornerG)
+        cv2.waitKey(25)
+        print(np.sum(corner))
+        if (np.sum(corner) < 500):
+            self.stop()
+        else:
+            self.lastCross = time.time()
+
+    def atCrosswalk(self, image):
+        h, w = image.shape[0:2]
+
+        for i in range(8, 10):
+            s = image[i*h/10, w/4:3*w/4, :]
+            # check how red the slice is
+            if np.sum(s[:, 2]) > 80000 and np.sum(s[:, 1]) < 80000:
+                cv2.circle(image, (w/2, i*h/10), 20, (255, 0, 0), -1)
+                return True
+        return False
 
     def follow(self, state):
-        if state < 15*self.slice_num/20:
+        if state < 25*self.slice_num/30:
+            self.move("LL")
+        elif state < 26*self.slice_num/30:
             self.move("L")
-        elif state > 18*self.slice_num/20:
+        elif state > 28*self.slice_num/30:
             self.move("R")
+        elif state > 29*self.slice_num/30:
+            self.move("RR")
         else:
             self.move("F")
 
