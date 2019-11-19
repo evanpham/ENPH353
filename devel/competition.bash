adaca2c381b447cf283b89054b108b7b511af72c
Evan Pham <pham@enph353.JoDo>
2019-11-19 11:48:30 -0800

    Can display bboxes on live camera feed

---
+++ devel/competition.bash
@@ -0,0 +1,3 @@
+source /setup.bash # path to the setup script for your controller worskpace
+export ROS_MASTER_URI=http://10.42.0.XXX:11311 #ROS Master socket on ENPH Competition computer
+export ROS_IP=128.189.219.140  # IP address of student computer
