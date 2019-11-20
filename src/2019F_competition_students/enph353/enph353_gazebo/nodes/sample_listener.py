610d137965c8bae186f726d495a1162b06e58e86
Evan Pham <pham@enph353.JoDo>
2019-11-19 21:02:05 -0800

    fixed holdover from previous plate readings

--- src/2019F_competition_students/enph353/enph353_gazebo/nodes/sample_listener.py
+++ src/2019F_competition_students/enph353/enph353_gazebo/nodes/sample_listener.py
@@ -20,33 +20,33 @@ plateReadings = np.empty((5, 4), dtype=str)
 
 def callback(data):
     global frameCount, plateReadings
-
+    # Increment frameCount
+    # Every this node gets 5 frames per car passed
     frameCount = frameCount + 1
+
     try:
         img = bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
         print(e)
     chars = getPlateChars(img)
 
+    # Add character guesses to plateReadings array
     for i in range(4):
         plateReadings[frameCount % 5, i] = chars[i]
 
-    # Gets plate characters from image
+    # After 5 frames, get modal guess for each character
     if (frameCount % 5 == 0):
+        goodRows = []
         for i in range(4, -1, -1):
-            if (plateReadings[i] == ['0', '0', '0', '0']).all():
-                plateReadings = np.delete(plateReadings, i, axis=0)
+            if not (plateReadings[i] == ['0', '0', '0', '0']).all():
+                goodRows.append(i)
         try:
-            bestGuess = str(mode(plateReadings)[0][0])
-            plate = ''.join(str(bestGuess))
+            bestGuess = ''.join(mode(plateReadings[goodRows, :])[0][0])
             print(plateReadings)
+            print(plateReadings[goodRows, :])
             print(bestGuess)
-            # cv2.putText(img, bestGuess, textLocation, font, fontScale, fontColor, lineType)
-            # cv2.imshow("img", img)
-            # cv2.waitKey(25)
         except IndexError:
             print("Could not read plate")
-        plateReadings = np.empty((5, 4), dtype=str)
 
 
 if __name__ == '__main__':
