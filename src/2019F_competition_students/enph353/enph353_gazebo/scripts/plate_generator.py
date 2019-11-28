37b09cc5aec76bf2e061abecf778feca7e95a348
gabrielrobinsonleith <gabriel@enph353.GaRo>
2019-11-27 16:41:29 -0800

    Changing plate generator back to what it should be

--- src/2019F_competition_students/enph353/enph353_gazebo/scripts/plate_generator.py
+++ src/2019F_competition_students/enph353/enph353_gazebo/scripts/plate_generator.py
@@ -22,7 +22,8 @@ with open(path + "plates.csv", 'w') as plates_file:
         # Pick two random letters
         plate_alpha = ""
 
-        plate_alpha += "TI"
+		for _ in range(0, 2):
+			plate_alpha += (random.choice(string.ascii_uppercase))
         num = randint(0, 99)
 
         # Pick two random numbers
